40cec8ae836607fc85d1c39a44511fd5f08a9c8f
Anitha Venkatesan <anithamca68@gmail.com>
2020-04-21 18:27:15 -0500

    Added array objects in functions

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ var answer4Btn= document.getElementById('answer4');
 function countDownTimer()
 {
     
-    readQuestions();;
+    readQuestions();
     var timerCount =75; 
     var Readtimer=setInterval(startTimer,1000);
     
@@ -73,47 +73,65 @@ startBtn.addEventListener('click',countDownTimer);
         {
           question: "What is the JavaScript syntax for printing values in Console?",
           answers: {
-            1: "print(5)",
-            2: "console.print(5);",
-            3: "console.log(5);",
-            4: "print.console(5);"},
+            1 : "print(5)",
+            2 : "console.print(5);",
+            3 : "console.log(5);",
+            4 : "print.console(5);"},
           correctAnswer: "3"
         },
         {
           question: "Which built-in method reverses the order of the elements of an array?",
           answers: {
-            1: "reverse()",
-            2: "changeOrder(order)",
-            3: "sort(order)",
-            4: "None of the above."}, 
+            1 : "reverse()",
+            2 : "changeOrder(order)",
+            3 : "sort(order)",
+            4 : "None of the above."}, 
           correctAnswer: "1"
         },
         {
           question: "What does HTML stand for?",
           answers: {
-              1: "Hyper Time Markup Language",
-              2: "Hyperlinks and Text Markup Language",
-              3: "Home Tool Markup Language",
-              4: "Hyper Text Markup Language"}, 
+              1 : "Hyper Time Markup Language",
+              2 : "Hyperlinks and Text Markup Language",
+              3 : "Home Tool Markup Language",
+              4 : "Hyper Text Markup Language"}, 
           correctAnswer: "4"
         },
         {
           question: "Which character is used to indicate an end tag?",
           answers: {
-              1: "/",
-              2: "<",
-              3: ">",
-              4: "*"}, 
+              1 : "/",
+              2 : "<",
+              3 : ">",
+              4 : "*"}, 
           correctAnswer: "1"
         },
         ];
-    var i=0;
+    
+    var i=0;var j=0;var k=0;
     console.log(myQuestions.length);
     
-    if(i<=myQuestions.length)
+    if(i<myQuestions.length)
     {
-      
       questionEl.innerHTML= (myQuestions[i].question);
+      var allChoicesArr=Object.keys(myQuestions[i].answers);
+      var allAnswersArr =Object.values(myQuestions[i].answers);
+      console.log(allChoicesArr.concat(allAnswersArr));
+      console.log(allAnswersArr);
+      if(j< allChoicesArr.length && k<allAnswersArr.length)
+      {
+          answer1Btn.innerHTML=allChoicesArr[j].concat(allAnswersArr[k]);
+          j++;k++;
+          answer2Btn.innerHTML=allChoicesArr[j].concat(allAnswersArr[k]);
+          j++;k++;
+          answer3Btn.innerHTML=allChoicesArr[j].concat(allAnswersArr[k]);
+          j++;k++;
+          answer4Btn.innerHTML=allChoicesArr[j].concat(allAnswersArr[k]);
+        
+      }
+      
+      
+  
       i++;
     }
     
