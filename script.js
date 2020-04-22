695d58f7483ccfd8e13c5c52526f3c5453c77eff
Anitha Venkatesan <anithamca68@gmail.com>
2020-04-22 15:42:50 -0500

    Fixed issues for button clicks for answers

--- script.js
+++ script.js
@@ -12,54 +12,34 @@ var answer4Btn= document.getElementById('answer4');
 
 function countDownTimer()
 {
-    
-    readQuestions();
-    var timerCount =75; 
-    var Readtimer=setInterval(startTimer,1000);
-    
+ readQuestions();
+ var timerCount =75; 
+ var Readtimer=setInterval(startTimer,1000);
     function startTimer()
     {
-      
-        if(timerCount >= 0)
-         {
+        if(timerCount >= 0){
           timerCount--;
           timerEl.textContent = "Time:" +timerCount;
-          if(sectionEl.style.display ==="none")
-          {
-            
+          if(sectionEl.style.display ==="none"){
             divEl.style.display="block";
             sectionEl.style.display="none";
           }
-          else
-          {
-            
+          else{
             sectionEl.style.display="block";
             divEl.style.display="none";
           }
-          
-          
         }
-       
         if (timerCount == 0) {
-          
           timerEl.textContent = "Time:0" +0;
           clearInterval(Readtimer); 
-
-          
-        }
-        
+        } 
     }
 } 
 
-
-  
-
 startBtn.addEventListener('click',countDownTimer);
- 
-  
+
  function readQuestions()
     {
-       
        var myQuestions = [
         {
           question: "What is the syntax for creating a function in JavaScript named as Geekfunc?",
@@ -68,7 +48,7 @@ startBtn.addEventListener('click',countDownTimer);
             "function Geekfunc()",
            "function := Geekfunc()",
             "function : Geekfunc()"],
-          correctAnswer: "2"
+          correctAnswer: "function Geekfunc()"
         },
         {
           question: "What is the JavaScript syntax for printing values in Console?",
@@ -77,7 +57,7 @@ startBtn.addEventListener('click',countDownTimer);
             "console.print(5);",
             "console.log(5);",
             "print.console(5);"],
-          correctAnswer: "3"
+          correctAnswer: "console.log(5);"
         },
         {
           question: "Which built-in method reverses the order of the elements of an array?",
@@ -86,7 +66,7 @@ startBtn.addEventListener('click',countDownTimer);
             "changeOrder(order)",
             "sort(order)",
             "None of the above."], 
-          correctAnswer: "1"
+          correctAnswer: "reverse()"
         },
         {
           question: "What does HTML stand for?",
@@ -95,7 +75,7 @@ startBtn.addEventListener('click',countDownTimer);
               "Hyperlinks and Text Markup Language",
               "Home Tool Markup Language",
               "Hyper Text Markup Language"], 
-          correctAnswer: "4"
+          correctAnswer: "Hyper Text Markup Language"
         },
         {
           question: "Which character is used to indicate an end tag?",
@@ -104,12 +84,11 @@ startBtn.addEventListener('click',countDownTimer);
               "<",
               ">",
               "*"], 
-          correctAnswer: "1"
+          correctAnswer: "/"
         },
         ];
-    
+        
     var i=0;
-    //console.log(myQuestions.length);
     var answerButton=[];
     
     if(i<myQuestions.length)
@@ -121,16 +100,26 @@ startBtn.addEventListener('click',countDownTimer);
           answerButton = allChoicesArr[j];
           var answer = document.getElementById('answer' + (j + 1));
           answer.innerHTML = (j + 1) + '. ' + answerButton;
-       
-      } 
-      
-      
+          console.log(answer);
+          if(myQuestions[i].correctAnswer === answerButton)
+          {
+            var answerDiv = document.createElement("div");
+            answer.addEventListener('click',function()
+            {
+            
+            answerDiv.innerHTML="Correct";
+            sectionEl.appendChild(answerDiv);
+            });
+          }
+          else{
+            answer.addEventListener('click',function()
+            {
+              answerDiv.innerHTML="Wrong";
+              sectionEl.appendChild(answerDiv);
+            });
+          }
+        }   
      
-      
-         
-        
-      
-      
       i++;
     }
     
