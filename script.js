508c235f49697c930c047fd1f18ddfd109931fe5
Anitha Venkatesan <anithamca68@gmail.com>
2020-04-22 19:34:22 -0500

    Added event listener functions for answers

--- script.js
+++ script.js
@@ -7,12 +7,30 @@ var answer1Btn=document.getElementById('answer1');
 var answer2Btn= document.getElementById('answer2');
 var answer3Btn=document.getElementById('answer3');
 var answer4Btn= document.getElementById('answer4');
-
-
+var answerDiv = document.createElement("div");
+//EventListener for click events
 startBtn.addEventListener('click',countDownTimer);
-var timerCount =0; 
+answer1Btn.addEventListener('click', function() {
+  checkAnswer(0);
+});
+answer2Btn.addEventListener('click',function() {
+  checkAnswer(1);
+});
+answer3Btn.addEventListener('click',function() {
+  checkAnswer(2);
+});
+answer4Btn.addEventListener('click',function() {
+  checkAnswer(3);
+});
+
+var questionIndex=0;
+var timerCount=0; 
+var allChoicesArr;
+
+//Timer function to start the timer
 function countDownTimer()
 {
+ 
  readQuestions();
  timerCount =75; 
  var Readtimer=setInterval(startTimer,1000);
@@ -21,10 +39,13 @@ function countDownTimer()
         if(timerCount >= 0){
           timerCount--;
           timerEl.textContent = "Time:" +timerCount;
-          if(sectionEl.style.display ==="none"){
+          if(sectionEl.style.display==="none"){
+           
+            
             divEl.style.display="block";
             sectionEl.style.display="none";
           }
+          
           else{
             sectionEl.style.display="block";
             divEl.style.display="none";
@@ -36,100 +57,119 @@ function countDownTimer()
         } 
     }
 } 
-
+var myQuestions = [
+  {
+    question: "What is the syntax for creating a function in JavaScript named as Geekfunc?",
+    answers: [
+      "function = Geekfunc()",
+      "function Geekfunc()",
+      "function := Geekfunc()",
+      "function : Geekfunc()"],
+    correctAnswer: 1
+  },
+  {
+    question: "What is the JavaScript syntax for printing values in Console?",
+    answers: [
+      "print(5)",
+      "console.print(5);",
+      "console.log(5);",
+      "print.console(5);"],
+    correctAnswer: 2
+  },
+  {
+    question: "Which built-in method reverses the order of the elements of an array?",
+    answers: [
+      "reverse()",
+      "changeOrder(order)",
+      "sort(order)",
+      "None of the above."], 
+    correctAnswer: 0
+  },
+  {
+    question: "What does HTML stand for?",
+    answers: [
+        "Hyper Time Markup Language",
+        "Hyperlinks and Text Markup Language",
+        "Home Tool Markup Language",
+        "Hyper Text Markup Language"], 
+    correctAnswer: 3
+  },
+  {
+    question: "Which character is used to indicate an end tag?",
+    answers: [
+        "/",
+        "<",
+        ">",
+        "*"], 
+    correctAnswer: 0
+  },
+  ];
 
  function readQuestions()
     {
-       var myQuestions = [
-        {
-          question: "What is the syntax for creating a function in JavaScript named as Geekfunc?",
-          answers: [
-            "function = Geekfunc()",
-            "function Geekfunc()",
-           "function := Geekfunc()",
-            "function : Geekfunc()"],
-          correctAnswer: "function Geekfunc()"
-        },
-        {
-          question: "What is the JavaScript syntax for printing values in Console?",
-          answers: [
-            "print(5)",
-            "console.print(5);",
-            "console.log(5);",
-            "print.console(5);"],
-          correctAnswer: "console.log(5);"
-        },
-        {
-          question: "Which built-in method reverses the order of the elements of an array?",
-          answers: [
-            "reverse()",
-            "changeOrder(order)",
-            "sort(order)",
-            "None of the above."], 
-          correctAnswer: "reverse()"
-        },
-        {
-          question: "What does HTML stand for?",
-          answers: [
-              "Hyper Time Markup Language",
-              "Hyperlinks and Text Markup Language",
-              "Home Tool Markup Language",
-              "Hyper Text Markup Language"], 
-          correctAnswer: "Hyper Text Markup Language"
-        },
-        {
-          question: "Which character is used to indicate an end tag?",
-          answers: [
-              "/",
-              "<",
-              ">",
-              "*"], 
-          correctAnswer: "/"
-        },
-        ];
-        
-    var i=0;
-    var answerButton=[];
-    
-    if(i<myQuestions.length)
-    {
-      questionEl.innerHTML= (myQuestions[i].question);
-      var allChoicesArr=(myQuestions[i].answers);
+      var answerButton=[];
+      questionEl.innerHTML= (myQuestions[questionIndex].question);
+      allChoicesArr=(myQuestions[questionIndex].answers);
+      //console.log(allChoicesArr);
       for(var j=0;j< allChoicesArr.length ;j++)
       {
           answerButton = allChoicesArr[j];
+          console.log(answerButton);
           var answer = document.getElementById('answer' + (j + 1));
+          
           answer.innerHTML = (j + 1) + '. ' + answerButton;
-          console.log(answer);
-          if(myQuestions[i].correctAnswer === answerButton)
-          {
-            var answerDiv = document.createElement("div");
-            answer.addEventListener('click',function()
-            {
-            
-            answerDiv.innerHTML="Correct";
-            sectionEl.appendChild(answerDiv);
-            });
-          }
-          else{
-            answer.addEventListener('click',function()
-            {
-              timerCount -=10;
-              timerEl.textContent = "Time:" +timerCount;
-              answerDiv.innerHTML="Wrong";
-              sectionEl.appendChild(answerDiv);
-            });
-          }
-        }   
-     
-      i++;
-      console.log(myQuestions[i]);
-      
+          
+          
+      }
+    }
+    function checkAnswer(btnIndex)
+    {
+      if(myQuestions[questionIndex].correctAnswer === btnIndex)
+      {
+          answerDiv.style.display="block";
+          answerDiv.innerHTML="Correct!";
+          sectionEl.appendChild(answerDiv);
+          var score =0;
+          score = score+5;
+          
+      }
+      else
+      {
+           timerCount -=10;
+           timerEl.textContent = "Time:" +timerCount;
+           answerDiv.style.display="block";
+           answerDiv.innerHTML="Wrong!";
+           sectionEl.appendChild(answerDiv);
+           
+      }
+      setTimeout(function(){ 
+        
+        questionIndex++;
+        answerDiv.style.display="none";
+        readQuestions();}, 1000);
+        
+    }
+    function result()
+    {
+      var score =0;
+
+
     }
     
-  }
-
-
-
     
+  
+    
+    
+ 
+      
+      
+      
+    
+  
+  
+     
+   
+    
+  
+
 
